f09f128afb50e29d7e92aadd288a009004166a26
Josh Colwell <jcolwell4@me.com>
2016-12-15 21:19:28 -0800

    Stubbed Front end. Created FiniteStateMachine class to help control front end menu logic.

---
+++ Assets/Scripts/FiniteStateMachine.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//============================================================
+// Class FiniteStateMachine
+//============================================================
+public class FiniteStateMachine<StateType>
+{
+    List<State> mStates;
+    State mCurrentState;
+
+    public FiniteStateMachine(int size, State state)
+    {
+        mStates = new List<State>(size);
+        mStates.Add(state);
+        mCurrentState = mStates[0];
+    }
+
+    ~FiniteStateMachine()
+    {
+        mStates.Clear();
+    }
+
+    public void AddState(State newState)
+    {
+        mStates.Add(newState);
+    }
+
+    public void Clear()
+    {
+        mStates.Clear();
+    }
+
+    public void ChangeState(int index)
+    {
+        Debug.Assert(index < mStates.Count);
+
+        mCurrentState.Exit();
+        mCurrentState = mStates[index];
+        mCurrentState.Enter();
+    }
+
+    public string GetCurrentStateTag()
+    {
+        return mCurrentState.GetTag();
+    }
+}
+//============================================================
+
+//============================================================
+// Class State
+//============================================================
+public abstract class State
+{
+    string mTag;
+
+    public State( string tag )
+    {
+        mTag = tag;
+    }
+
+    public string GetTag() { return mTag; }
+
+    public virtual void Enter() { }
+
+    public virtual void Exit() { }
+
+    public virtual void Update( float deltaTime ) { }
+}
+//============================================================
+
+//============================================================
+// Class StateNone
+//============================================================
+public class StateNone : State
+{
+    public StateNone() : base("No Active State")
+    {
+    }
+}
+//============================================================
---
+++ Assets/Scripts/FrontEndMenu.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//============================================================
+// Class FrontEndMenu
+//============================================================
+public class FrontEndMenu : MonoBehaviour
+{
+    public GameObject campaignSubMenu;
+    public GameObject encounterSubMenu;
+    public GameObject settingsSubMenu;
+
+    private FiniteStateMachine<State> mFSM;
+    
+    enum MenuStates : int
+    {
+        none = 0,
+        campaign,
+        encounter,
+        settings
+    };
+
+    //------------------------------------------------------------
+
+    public void Start()
+    {
+        mFSM = new FiniteStateMachine<State>( 4, new StateNone());
+        mFSM.AddState(new CampaignMenuState("CampaignSubMenu",   campaignSubMenu));
+        mFSM.AddState(new EncounterMenuState("EncounterSubMenu", encounterSubMenu));
+        mFSM.AddState(new SettingsMenuState("SettingSubMenu",    settingsSubMenu));
+
+        mFSM.ChangeState( (int)MenuStates.none );
+    }
+
+    //------------------------------------------------------------
+
+    public void OnCampaignClick()
+    {
+        mFSM.ChangeState((int)MenuStates.campaign);
+    }
+
+    public void OnEncounterClick()
+    {
+        mFSM.ChangeState((int)MenuStates.encounter);
+    }
+
+    public void OnSettingsClick()
+    {
+        mFSM.ChangeState((int)MenuStates.settings);
+    }
+
+    //------------------------------------------------------------
+
+    public void OnCampaignCreate()
+    {
+        Debug.Log("[FrontEndMenu] OnCampaignCreate() is not yet implemented! ");
+    }
+
+    public void OnCampaignLoad()
+    {
+        Debug.Log("[FrontEndMenu] OnCampaignLoad() is not yet implemented! ");
+    }
+
+    public void OnCampaignConnect()
+    {
+        Debug.Log("[FrontEndMenu] OnCampaignConnect() is not yet implemented! ");
+    }
+
+    //------------------------------------------------------------
+
+    public void OnEncounterCreate()
+    {
+        Debug.Log("[FrontEndMenu] OnEncounterCreate() is not yet implemented! ");
+    }
+
+    public void OnEncounterLoad()
+    {
+        Debug.Log("[FrontEndMenu] OnEncounterLoad() is not yet implemented! ");
+    }
+
+    //------------------------------------------------------------
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+}
+//============================================================
+
+//============================================================
+// Front End Menu States
+//============================================================
+
+public class CampaignMenuState : State
+{
+    GameObject campaignSubMenu;
+
+    public CampaignMenuState( string tag, GameObject gameObject ) : base( tag )
+    {
+        campaignSubMenu = gameObject;
+    }
+
+    public override void Enter()
+    {
+        campaignSubMenu.SetActive( !campaignSubMenu.activeInHierarchy );
+    }
+
+    public override void Exit()
+    {
+        campaignSubMenu.SetActive(false);
+    }
+}
+
+//------------------------------------------------------------
+
+public class EncounterMenuState : State
+{
+    public GameObject encounterSubMenu;
+
+    public EncounterMenuState(string tag, GameObject gameObject) : base(tag)
+    {
+        encounterSubMenu = gameObject;
+    }
+
+    public override void Enter()
+    {
+        encounterSubMenu.SetActive(!encounterSubMenu.activeInHierarchy);
+    }
+
+    public override void Exit()
+    {
+        encounterSubMenu.SetActive(false);
+    }
+}
+
+//------------------------------------------------------------
+
+public class SettingsMenuState : State
+{
+    GameObject settingSubMenu;
+
+    public SettingsMenuState(string tag, GameObject gameObject) : base(tag)
+    {
+        settingSubMenu = gameObject;
+    }
+
+    public override void Enter()
+    {
+        settingSubMenu.SetActive(!settingSubMenu.activeInHierarchy);
+    }
+
+    public override void Exit()
+    {
+        settingSubMenu.SetActive(false);
+    }
+}
+
+//============================================================
